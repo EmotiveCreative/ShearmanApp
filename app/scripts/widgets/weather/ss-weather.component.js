fcc0c12d9a82beb02c5b53532da841f993ffa55c
lenny <Lenny@Clarity.fios-router.home>
2017-03-10 12:49:50 -0500

    Added dynamic image check to weather widget UI based on weather conditions returned from OpenWeather API.

--- app/scripts/widgets/weather/ss-weather.component.js
+++ app/scripts/widgets/weather/ss-weather.component.js
@@ -21,6 +21,7 @@
 
             if (!ssWeatherData || (ssWeatherData && (currentTimestamp - ssWeatherData.timestamp > 600))) {
                 ssWeatherDataService.getWeatherData(vm.cityIds).then(function (response) {
+                    response.data.list = addWeatherImageUrls(response.data.list);
                     vm.weatherData = ssWeatherData = response.data;
                     ssWeatherData.timestamp = currentTimestamp;
                     localStorage.setItem('ssWeatherData', JSON.stringify(ssWeatherData));
@@ -31,6 +32,47 @@
             vm.weatherData = ssWeatherData;
         
         };
+
+        function addWeatherImageUrls(weatherDataArr) {
+            if (weatherDataArr.length && weatherDataArr.length > 0) {
+                weatherDataArr.forEach(function(weatherItem) {
+                    // source: https://openweathermap.org/weather-conditions
+                    switch (weatherItem.weather[0].main) {
+                        case "Thunderstorm":
+                            weatherItem.imageUrl = '/images/004-flash.svg';
+                            break;
+                        case "Drizzle":
+                            weatherItem.imageUrl = '/images/006-umbrella.svg';
+                            break;
+                        case "Rain":
+                            weatherItem.imageUrl = '/images/006-umbrella.svg';
+                            break;
+                        case "Snow":
+                            weatherItem.imageUrl = '/images/008-snowflake.svg';
+                            break;
+                        case "Atmosphere":
+                            weatherItem.imageUrl = '/images/006-umbrella.svg';
+                            break;
+                        case "Clear":
+                            weatherItem.imageUrl = '/images/003-sun.svg';
+                            break;
+                        case "Clouds":
+                            weatherItem.imageUrl = '/images/007-cloud-outline.svg';
+                            break;
+                        case "Extreme":
+                            weatherItem.imageUrl = '/images/004-flash.svg';
+                            break;
+                        case "Additional":
+                            weatherItem.imageUrl = '/images/007-cloud-outline.svg';
+                            break;
+                        default:
+                            weatherItem.imageUrl = '/images/003-sun.svg';
+                            break;
+                    }
+                }); 
+            }
+            return weatherDataArr;
+        }
 
     }
 
