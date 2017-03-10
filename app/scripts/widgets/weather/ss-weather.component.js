5f6aa0fb5835ae0288093808284d97bddf6ab7cf
lenny <Lenny@Clarity.fios-router.home>
2017-03-10 10:21:44 -0500

    Implemented caching mechanism for weather data retrieved from API.

--- app/scripts/widgets/weather/ss-weather.component.js
+++ app/scripts/widgets/weather/ss-weather.component.js
@@ -8,22 +8,30 @@
             },
             controller: Controller,
             controllerAs: 'ssWeather',
-            templateUrl:'scripts/widgets/weather/ss-weather.component.html'
+            templateUrl: 'scripts/widgets/weather/ss-weather.component.html'
         });
 
 
     function Controller(ssWeatherDataService) {
         var vm = this;
 
-        vm.$onChanges = function() {
+        vm.$onChanges = function () {
+            var ssWeatherData = JSON.parse(localStorage.getItem('ssWeatherData'));
+            var currentTimestamp = Math.round(new Date() / 1000);
 
-            // ssWeatherDataService.getWeatherData(vm.cityIds).then(function(response) {
-            //     vm.weatherData = response.data;
-            //     console.log(vm.weatherData);
-            // });
-            
-        };
+            if (!ssWeatherData || (ssWeatherData && (currentTimestamp - ssWeatherData.timestamp > 600))) {
+                ssWeatherDataService.getWeatherData(vm.cityIds).then(function (response) {
+                    vm.weatherData = ssWeatherData = response.data;
+                    ssWeatherData.timestamp = currentTimestamp;
+                    localStorage.setItem('ssWeatherData', JSON.stringify(ssWeatherData));
+                });
+                return;
+            }
+
+            vm.weatherData = ssWeatherData;
         
+        };
+
     }
 
 })();
